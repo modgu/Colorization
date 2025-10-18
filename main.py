1b4f7cca0292a5c42bc975f990f52df1dc36073b
Modgo <abdelaldev@gmail.com>
2025-10-18 05:51:36 +0200

    new changes

--- main.py
+++ main.py
@@ -167,4 +167,4 @@ def end_session():
     return "Session ended and all images deleted."
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
